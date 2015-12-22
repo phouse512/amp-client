0100dda233eebb08d1962de18e7a6f8c98387cf1
phouse512 <philiphouse2015@u.northwestern.edu>
2015-12-22 01:04:05 -0600

    fleshing out client

--- README.md
+++ README.md
@@ -4,7 +4,16 @@ flask application built for raspbian
 ## Design Goals:
 - no internet connection
 - can be pinged/identity requested across lan
-- can divulge status through
+- can divulge status
+- write actions to local disk cache
+- to be published
 
 
 ## ## user goals
+- plug-and-play: minimal effort on the user side, plug in and go
+
+
+
+## master goals:
+- scan on button press
+-
---
+++ client.py
@@ -0,0 +1,35 @@
+from flask import Flask
+#import RPi.GPIO as GPIO
+
+ALLOWED_PINS = [21, 26]
+
+def pi_initialization():
+	# GPIO.setmode(GPIO.BCM)
+	print "mocking the setup of GPIO pins"
+
+# initialize the world here:
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return "Hello World!"
+
+
+@app.route("/status")
+def status():
+
+
+@app.route("/toggle/<int:pin_out>")
+def toggle_pin(pin_out):
+	if pin_out not in ALLOWED_PINS:
+		return 'invalid pin specified'
+
+
+	GPIO.output(pin_out)
+
+
+if __name__ == "__main__":
+	pi_initialization()
+	app.run(host='0.0.0.0', port=5031)
+
+

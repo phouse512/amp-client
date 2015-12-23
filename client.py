d8ee88f6daf03fa15b239b6db204f58b825a927e
phouse512 <philiphouse2015@u.northwestern.edu>
2015-12-23 17:51:02 -0500

    set up client server to load details from yml files

--- client.py
+++ client.py
@@ -1,9 +1,12 @@
+import yaml
+
 from flask import Flask
 from gpio import InvalidRaspberryPiEnvironment
 from gpio import RaspGPIO
 from gpio import UnixGPIO
 
 from sys import argv
+
 
 ALLOWED_PINS = [21, 26]
 pi_client = None
@@ -26,8 +29,7 @@ def hello():
 
 @app.route("/status")
 def status():
-	print pi_client
-	return "status"
+	return str(server_conf['server_id'])
 
 @app.route("/toggle/<int:pin_out>")
 def toggle_pin(pin_out):
@@ -42,6 +44,11 @@ if __name__ == "__main__":
 	try:
 		script, environment = argv
 		pi_client = pi_initialization(environment)
+
+		# load from file server id, if not there, create one
+		with open("conf/client_conf.yml", 'r') as stream:
+			server_conf = yaml.load(stream)
+
 		app.run(host='0.0.0.0', port=5031)
 	except KeyboardInterrupt as e:
 		print "fielded keyboard interrupt. Closing now."
